5e98542ace0938fc1b52280cc2cdbeedab7099bf
Djurgarden2019 <sags3458@gmail.com>
2026-08-12 19:15:12 +0200

    Main 101: wire quick navigation anchors

--- app/live/page.tsx
+++ app/live/page.tsx
@@ -2,6 +2,7 @@ import LiveRadarBrowser from "../../components/LiveRadarBrowser";
 import EditorialDesk19 from "../../components/EditorialDesk19";
 import SourceVerification20 from "../../components/SourceVerification20";
 import SourceHunter21 from "../../components/SourceHunter21";
+import LiveQuickNav101 from "../../components/LiveQuickNav101";
 
 export default function LivePage() {
   return (
@@ -16,10 +17,11 @@ export default function LivePage() {
             Main 19 prioriterar kandidater och kräver källkontroll innan publicering.
           </p>
         </section>
-        <EditorialDesk19 />
-        <SourceVerification20 />
-        <SourceHunter21 />
-        <LiveRadarBrowser />
+        <LiveQuickNav101 />
+        <div id="redaktionsbordet" style={{scrollMarginTop:'24px'}}><EditorialDesk19 /></div>
+        <div id="kallkontroll" style={{scrollMarginTop:'24px'}}><SourceVerification20 /></div>
+        <div id="kalljakt" style={{scrollMarginTop:'24px'}}><SourceHunter21 /></div>
+        <div id="liveflodet" style={{scrollMarginTop:'24px'}}><LiveRadarBrowser /></div>
       </div>
     </main>
   );
