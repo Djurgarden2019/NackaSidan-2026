1602489f496c86e191784a69f03c7cf82ce1aa8c
Djurgarden2019 <sags3458@gmail.com>
2026-08-12 17:02:47 +0200

    Main 87: koppla in tablet-layout för nyhetsradarn

--- app/layout.tsx
+++ app/layout.tsx
@@ -6,6 +6,7 @@ import './live-frontpage-sprint83.css';
 import './live-frontpage-sprint84.css';
 import './live-frontpage-sprint85.css';
 import './live-frontpage-sprint86.css';
+import './live-frontpage-sprint87.css';
 import { Header } from '../components/Header';
 import { Footer } from '../components/Footer';
 
