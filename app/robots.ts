8743ae459ada1b4ff2802c217dd378eb20c59e13
Djurgarden2019 <sags3458@gmail.com>
2026-08-17 14:08:34 +0200

    Main 279: add robots policy and sitemap discovery

---
+++ app/robots.ts
@@ -0,0 +1,13 @@
+import type { MetadataRoute } from 'next';
+
+export default function robots():MetadataRoute.Robots{
+ return {
+  rules:{
+   userAgent:'*',
+   allow:'/',
+   disallow:['/redaktion','/inloggning','/artikelverkstaden','/forhandsgranskning','/driftpanel','/behorigheter'],
+  },
+  sitemap:'https://nacka-sidan-2026-delta.vercel.app/sitemap.xml',
+  host:'https://nacka-sidan-2026-delta.vercel.app',
+ };
+}
